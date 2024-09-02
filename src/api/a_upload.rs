54039ccbd2b3fedc0f910884fcb782a50fcc20bc
BlueGecko <kei09doya@gmail.com>
2024-09-02 16:24:42 +0900

    changed internal processing of upload api

--- src/api/a_upload.rs
+++ src/api/a_upload.rs
@@ -6,28 +6,19 @@ use crate::SAVE_DIR;
 
 #[instrument]
 pub async fn upload(mut multipart: Multipart) -> Result<(), StatusCode> {
-    while let Some(field) = multipart.next_field().await.unwrap() {
+    while let Some(mut field) = multipart.next_field().await.unwrap() {
         let filename = match field.file_name() {
             Some(name) => name.to_owned(),
             None => return Err(StatusCode::BAD_REQUEST),
         };
-        match field.bytes().await {
-            Ok(data) => {
-                event!(Level::INFO, "Uploading {}, {}bytes", filename, data.len());
-                let mut file = File::create(format!("{}/{}", SAVE_DIR.clone(), filename))
-                    .await
-                    .unwrap();
-                file.write_all(&data).await.unwrap();
-            }
-            Err(e) => {
-                event!(
-                    Level::ERROR,
-                    "An error occurred while receiving the uploaded file: Filename: {}, {}",
-                    filename,
-                    e
-                );
-                return Err(StatusCode::INTERNAL_SERVER_ERROR);
-            }
+        event!(Level::INFO, "Start receive {}", filename);
+
+        let mut file = File::create(format!("{}/{}", SAVE_DIR.clone(), filename))
+            .await
+            .unwrap();
+
+        while let Some(chunk) = field.chunk().await.unwrap() {
+            file.write_all(&chunk).await.unwrap();
         }
     }
     Ok(())
